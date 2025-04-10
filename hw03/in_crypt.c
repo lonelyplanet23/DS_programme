b6cbf8651b070b6f1386a7f826e74d63f18b6531
naonao <276286687@qq.com>
2025-04-10 21:43:34 +0800

    4/10-first commit

---
+++ hw03/in_crypt.c
@@ -0,0 +1,111 @@
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+#include<ctype.h>
+#include<stdlib.h>
+#include<string.h>
+#define ll long long
+#define N 100001
+double eps = 1e-9;
+FILE *fin, *fout;
+typedef struct node
+{
+    char ch;
+    struct node *next;
+} *nodeptr;
+int used[128]; //表示字符是否使用过
+char str_without_repeat[128]; //存储不重复的字符
+char encrypt[128]; //存储加密后的字符
+void remove_duplicate(FILE *fin)
+{
+    char ch;
+    int k = 0;
+    while((ch = fgetc(fin)) != EOF)
+    {
+        if(ch == '\n') continue;
+        if(ch >= 32 && ch <= 126) //判断是否是可打印字符
+        {
+            if(used[ch] == 0) //如果没有使用过
+            {
+                str_without_repeat[k++] = ch; //存储不重复的字符
+            }
+            else continue; //如果使用过则跳过
+        }
+    }
+    str_without_repeat[k] = '\0'; //字符串结束符
+}
+nodeptr create_list(char *str_without_repeat)
+{
+    nodeptr head = (nodeptr)malloc(sizeof(struct node)); //创建头结点
+    head->next = NULL;
+    nodeptr p = head;
+    for(int i = 0; str_without_repeat[i] != '\0'; i++)
+    {
+        nodeptr new_node = (nodeptr)malloc(sizeof(struct node)); //创建新节点
+        new_node->ch = str_without_repeat[i]; //存储字符
+        new_node->next = NULL; //指向NULL
+        p->next = new_node; //将新节点连接到链表中 
+        p = new_node; //移动指针到新节点
+    }
+    for(int i = 32; i <= 126; i++)
+    {
+        if(used[i] == 0) //如果没有使用过
+        {
+            nodeptr new_node = (nodeptr)malloc(sizeof(struct node)); //创建新节点
+            new_node->ch = i; //存储字符
+            new_node->next = NULL; 
+            p->next = new_node; 
+            p = new_node; 
+        }
+    }
+    p -> next = head;
+    return p; //返回尾结点
+}
+nodeptr delete(nodeptr prv, nodeptr cur)
+{
+    prv->next = cur->next;
+    free(cur);
+    return prv->next;
+}
+nodeptr moving(nodeptr cur, int n)
+{
+    nodeptr p = cur;
+    for(int i = 1; i < n-1; i++) //可优化n
+    {
+        p= p->next;
+    }
+    return p; //返回目标指针的前一个
+}
+void encrypt_list(nodeptr tail, char *encrypt)
+{
+    nodeptr p = tail, q = tail->next; //指向第一个节点
+    int i = 32;
+    char first = p->ch;
+    q = q->next;
+    while(p != q)
+    {
+        int movingTimes = (int)p->ch;
+        p = delete(p,); //head这时无所指
+
+
+    }
+
+}
+int main()
+{
+    fin = fopen("in.txt", "r"); //打开输入文件
+    fout = fopen("out.txt", "w"); //打开输出文件
+    remove_duplicate(fin); //去除重复字符
+    nodeptr list = create_list(str_without_repeat); 
+    encrypt_list(list, encrypt); //加密字符
+    char cur;
+    while((cur = fgetc(fin) )!= EOF)
+    {
+        if(cur == '\n') fputc('\n', fout); 
+        if(cur >= 32 && cur <= 126) 
+        {
+            fputc(encrypt[cur], fout); 
+        }
+    }
+    return 0;
+}
---
+++ hw03/memory.c
@@ -0,0 +1,176 @@
+//内存申请
+//1）从当前位置开始遍历空闲块链表（初始是从地址最小的第一个空闲块开始），寻找满足条件的最小块（即：大于等于请求空间的最小空闲块，如果有多个大小相同的最小空闲块，则选择遍历遇到的第一个空闲块）（最佳适应原则）；
+//2）如果选择的空闲块恰好与请求的大小相符合，则将它从链表中移除并返回给用户；这时当前位置变为移除的空闲块指向的下一空闲块；
+//3）如果选择的空闲块大于所申请的空间大小，则将大小合适的空闲块返回给用户，剩下的部分留在空闲块链表中；这时当前位置仍然为该空闲块；
+//4）如果找不到足够大的空闲块，则申请失败；这时当前位置不变。
+//! 链表中没有空闲块，则什么都不输出，容易出现内存泄漏，必须处理
+#include<math.h>
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+#include<stdlib.h>
+#define ll long long
+#define N 100001
+double eps = 1e-9;
+typedef struct node
+{
+    int pos;
+    int len;
+    struct node *next;
+} *Nodeptr;
+Nodeptr create_list(int n)
+{
+    if (n <= 0) {
+        printf("无效的链表大小\n");
+        return NULL;
+    }
+    if (n <= 0) return NULL;
+    Nodeptr head = NULL, p, q;
+    int pos, len;
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d %d", &pos, &len);
+        q = (Nodeptr) malloc(sizeof(struct node));
+        if (q == NULL)
+        {
+            printf("内存分配失败\n");
+            exit(1);
+        }
+        q->len = len;
+        q->pos = pos;
+        q->next = NULL;
+        if(head == NULL)
+        {
+            head = p = q;
+            continue;
+        }
+        else
+        {
+            p->next = q;
+            p = p->next;// p = q;
+        }
+    }
+    // Creating a circular linked list by pointing the last node to the head
+    p->next = head;
+    return p;
+}
+Nodeptr allocate_memory(Nodeptr dst_prv, Nodeptr dst, int len)
+{
+    if(dst_prv == NULL || dst == NULL) return NULL;
+
+    if(dst->len == len)
+    {
+        if (dst_prv == dst) {
+            // 链表中只有一个节点
+            free(dst);
+            return NULL;
+        }
+        //删除该区块
+        dst_prv -> next = dst -> next;
+        free(dst);
+        return dst_prv;
+    }
+    else if(dst->len > len)
+    {
+        dst->len -= len;
+        return dst_prv;
+    }
+    return NULL;
+}
+Nodeptr apply_for_memmory(Nodeptr prv, int len)
+{
+    if (prv == NULL || prv->next == NULL) {
+        // 链表为空或只有一个节点且被完全分配
+        return prv;
+    }
+    Nodeptr cur = prv->next;
+    Nodeptr best_fit_prv = NULL;
+    Nodeptr p = prv;
+    do
+    {
+        if(best_fit_prv == NULL)
+        {
+            if(p->next->len == len)
+            {
+                best_fit_prv = p;
+                break;
+            }
+            else if(p->next->len > len && best_fit_prv == NULL)
+            {
+                best_fit_prv = p;
+            }
+            else if(p->next->len < len)
+            {
+                p = p->next;
+                continue;
+            }
+        }
+
+        if(p->next->len == len)
+        {
+            best_fit_prv = p;
+            break;
+        }
+        else if(p->next->len >= len && p->next->len < best_fit_prv->next->len) //严格小于
+        {
+            best_fit_prv = p;
+        }
+        p = p->next;
+    } while(p->next != cur);
+    if(best_fit_prv == NULL)
+    {
+        return prv;
+    }
+    Nodeptr next = allocate_memory(best_fit_prv, best_fit_prv->next, len);
+    return next;
+
+}
+void print(Nodeptr head)
+{
+    if (head == NULL) {
+        return;
+    }
+    Nodeptr p = head->next;
+    do
+    {
+        printf("%d %d\n", p->pos, p->len);
+        p = p->next;
+    } while(p != head->next);
+}
+void free_list(Nodeptr head)
+{
+    if (head == NULL) return;
+
+    Nodeptr p = head->next;
+    Nodeptr temp;
+
+    // 遍历链表并释放每个节点
+    while (p != head)
+    {
+        temp = p;
+        p = p->next;
+        free(temp);
+    }
+    free(head); // 最后释放头节点
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    Nodeptr prv = create_list(n);
+    int apply_len;
+    scanf("%d", &apply_len);
+    while (apply_len != -1)
+    {
+        prv = apply_for_memmory(prv, apply_len);
+        scanf("%d", &apply_len);
+    }
+
+    print(prv);
+
+    // 释放链表内存
+    free_list(prv);
+
+    return 0;
+}
